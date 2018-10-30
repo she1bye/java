fbf01214ddc86fbaab9fbf8ddecd2405adae1c8c
qingguoyx <1921764112@qq.com>
2018-10-30 17:17:26 +0800

    计算类

---
+++ src/main/java/calculator/He.java
@@ -0,0 +1,15 @@
+package calculator;
+
+import org.apache.log4j.Logger;
+
+
+public class He {
+	private static Logger logger = Logger.getLogger(He.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.info("hek");
+
+	}
+
+}
